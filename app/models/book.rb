ffec921bb54ed22392c2b3a7da861b56b7fd538a
toshiaki0311 <bigbird19980311@gmail.com>
2022-03-28 21:01:23 +0900

    gem 'annotateの導入'

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id                      :integer          not null, primary key
+#  author                  :string
+#  publisher               :string
+#  status                  :string
+#  summary                 :text
+#  the_date_of_publication :string
+#  title                   :text
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#
 class Book < ApplicationRecord
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id                      :integer          not null, primary key
+#  author                  :string
+#  publisher               :string
+#  status                  :string
+#  summary                 :text
+#  the_date_of_publication :string
+#  title                   :text
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
