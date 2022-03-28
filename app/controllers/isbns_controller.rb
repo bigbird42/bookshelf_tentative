069e17a8080da2451728558babb370cb0e54ba10
toshiaki0311 <bigbird19980311@gmail.com>
2022-03-28 21:13:54 +0900

    isbn機能のコントローラとビューファイルとパスの作成(簡単に)

---
+++ app/controllers/isbns_controller.rb
@@ -0,0 +1,2 @@
+class IsbnsController < ApplicationController
+end
---
+++ test/controllers/isbns_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class IsbnsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
